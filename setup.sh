fec7cc9057bae6fed8b0c0d09c4a9fa1c4d74e68
Hisiter <leeyueshing19@gmail.com>
2021-01-13 10:56:48 +0700

    update 5

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 pip install -r requirements.txt
 
 npm install localtunnel
-cd center/models/
 
+cd center/models/
 rm -rf DCNv2
 git clone https://github.com/CharlesShang/DCNv2.git
 cd DCNv2
@@ -10,10 +10,8 @@ sh make.sh
 python testcuda.py
 cd ../../../
 
-
 wget https://www.dropbox.com/s/tj96fsm6t6rq8ye/model-r100-arcface-ms1m-refine-v2.zip?dl=0 -O model-r100-arcface-ms1m-refine-v2.zip
-mkdir models/model-r100-ii
-unzip model-r100-arcface-ms1m-refine-v2.zip - d models
+unzip model-r100-arcface-ms1m-refine-v2.zip -d models
 rm model-r100-arcface-ms1m-refine-v2.zip
 
 gdown https://drive.google.com/uc?id=1igdw06UTw1SZxGa7JUCB-Vm3YiWl1cpt
